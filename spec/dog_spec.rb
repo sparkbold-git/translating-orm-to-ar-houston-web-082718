7145c9fd6494631bf50152db7a91bdad1273f263
sparkbold-git <sparkboldstudio@gmail.com>
2018-09-10 12:18:44 -0500

    Done.

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -1,40 +1,39 @@
-require "spec_helper"
-require "pry"
+require 'spec_helper'
+require 'pry'
 
-describe "Dog" do
-
-  let(:teddy) {Dog.new(name: "Teddy", breed: "cockapoo")}
+describe 'Dog' do
+  let(:teddy) { Dog.new(name: 'Teddy', breed: 'cockapoo') }
 
   before do
-    DB.execute("DROP TABLE IF EXISTS dogs")
+    DB.execute('DROP TABLE IF EXISTS dogs')
     @sql_runner = SQLRunner.new(DB)
     @sql_runner.execute_schema_migration_sql
   end
 
-  describe "inheritence" do
+  describe 'inheritence' do
     it 'inherits from ActiveRecord::Base' do
       expect(Dog.superclass).to eq(ActiveRecord::Base)
     end
   end
 
-  describe "attributes" do
+  describe 'attributes' do
     it 'has a name and a breed' do
-      dog = Dog.new({name: "Fido", breed: "lab"})
-      expect(dog.name).to eq("Fido")
-      expect(dog.breed).to eq("lab")
+      dog = Dog.new(name: 'Fido', breed: 'lab')
+      expect(dog.name).to eq('Fido')
+      expect(dog.breed).to eq('lab')
     end
   end
 
   describe '.create' do
     it 'takes in a hash of attributes and uses metaprogramming to create a new dog object. Then it uses the #save method to save that dog to the database' do
-      dog = Dog.create(name: "Ralph", breed: "lab")
-      expect(dog.name).to eq("Ralph")
+      dog = Dog.create(name: 'Ralph', breed: 'lab')
+      expect(dog.name).to eq('Ralph')
     end
   end
 
   describe '.save' do
     it 'saves an instance of the dog class to the database and then sets the given dogs `id` attribute' do
-      dog = Dog.new({name: "Fido", breed: "lab"})
+      dog = Dog.new(name: 'Fido', breed: 'lab')
       dog.save
       expect(dog.id).to eq(1)
     end
@@ -43,8 +42,8 @@ describe "Dog" do
   describe '.update' do
     it 'updates the record associated with a given instance' do
       teddy.save
-      teddy.update({name: "Teddy Jr."})
-      teddy_jr = Dog.find_by_name("Teddy Jr.")
+      teddy.update(name: 'Teddy Jr.')
+      teddy_jr = Dog.find_by_name('Teddy Jr.')
       expect(teddy_jr.id).to eq(teddy.id)
     end
   end
@@ -60,8 +59,8 @@ describe "Dog" do
   describe '.find_by_name' do
     it 'returns a dog that matches the name from the DB' do
       teddy.save
-      teddy_from_db = Dog.find_by_name("Teddy")
-      expect(teddy_from_db.name).to eq("Teddy")
+      teddy_from_db = Dog.find_by_name('Teddy')
+      expect(teddy_from_db.name).to eq('Teddy')
       expect(teddy_from_db).to be_an_instance_of(Dog)
     end
   end
@@ -74,5 +73,6 @@ describe "Dog" do
       expect(teddy_from_db).to be_an_instance_of(Dog)
     end
   end
-
 end
+binding.pry
+0
